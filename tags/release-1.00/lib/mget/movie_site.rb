e3530633144cf1353bb0d9f322917933e029c211
netprobe <netprobe@1632104f-0320-0410-8905-c31b79b9ab67>
2006-10-27 14:21:12 +0000

    Tagging the 1.00 release of the 'mget' project.

---
+++ tags/release-1.00/lib/mget/movie_site.rb
@@ -0,0 +1,78 @@
+# This file is part of the 'Movie Get' project.
+#
+# Copyright (C) 2006 Adam Wolk "mulander" <netprobe@gmail.com>
+#                                "defc0n" <defc0n@da-mail.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'mget/error_handling'
+require 'open-uri'
+require 'net/http'
+require 'uri'
+
+class MovieSite
+  include ErrorHandling
+  attr_reader :suffix
+  def initialize(url,config)
+    @log = Logger.new(self.class.to_s + '.log')
+    @error    = nil
+    @url      = url
+    @cookie   = ''
+    @loggedIn = false
+    @suffix   = '.flv'
+    @useragent= 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.1) Gecko/20060111'
+    unless config.nil?
+      @username = config['username']
+      @password = config['password']
+    else
+      @username = nil
+      @password = nil
+    end
+  end
+  
+  private
+  def passwordSet?
+    return (@password.nil? || @password.empty?) ? false : true
+  end
+  def usernameSet?
+    return (@username.nil? || @username.empty?) ? false : true
+  end
+  
+  def loggedIn?
+    return @loggedIn
+  end
+ 
+  def askPassword
+    print "#{ self.class } password: "
+    @password = $stdin.gets.chomp
+  end
+  
+  def askUsername
+    print "#{ self.class } username: "
+    @username = $stdin.gets.chomp
+  end  
+
+  def askAccountInfo
+    puts "#{ self.class } decided that in order to view this movie you must identify"
+    until usernameSet?
+      askUsername()
+    end
+    
+    until passwordSet?
+      askPassword()
+    end
+  end
+
+end
---
+++ tags/release-1.00/mget.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/ruby -w
+# v1.00
+#
+# Copyright (C) 2006 Adam Wolk "mulander" <netprobe@gmail.com>
+#                                "defc0n" <defc0n@da-mail.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+$LOAD_PATH.push('lib')
+
+require 'getoptlong'
+require 'mget/mget'
+
+module Config
+  @@youtube = { 'username' => '', 'password' => '' }
+end
+
+class Mget
+  include Config
+end
+
+opts = GetoptLong.new(
+  ["--help","-h",       GetoptLong::NO_ARGUMENT],
+  ["--name","-n",       GetoptLong::REQUIRED_ARGUMENT],
+  ["--input","-i",      GetoptLong::REQUIRED_ARGUMENT],
+  ["--download","-d",   GetoptLong::NO_ARGUMENT],
+  ["--nodownload","-D", GetoptLong::NO_ARGUMENT],
+  ["--convert","-c",    GetoptLong::NO_ARGUMENT],
+  ["--remove","-r",     GetoptLong::NO_ARGUMENT],
+  ["--noremove","-R",   GetoptLong::NO_ARGUMENT],
+  ["--noconvert","-C",  GetoptLong::NO_ARGUMENT],
+  ["--show","-s",       GetoptLong::NO_ARGUMENT],
+  ["--quiet","-q",      GetoptLong::NO_ARGUMENT],
+  ["--version","-v",      GetoptLong::NO_ARGUMENT]
+)
+
+opts.ordering = GetoptLong::REQUIRE_ORDER
+
+mget = Mget.new()
+begin
+  opts.each do |opt, arg|
+    case opt
+      when '--help'
+        Mget.help()
+      when '--name'
+        mget.name   = arg
+      when '--input'
+        mget.input  = arg
+      when '--download'
+        mget.download = true
+      when '--nodownload'
+        mget.download = false
+        mget.show     = true
+      when '--convert'
+        mget.convert  = true
+      when '--noconvert'
+        mget.convert  = false
+      when '--remove'
+        mget.remove   = true
+      when '--noremove'
+        mget.remove   = false
+      when '--show'
+        mget.show     = true
+      when '--quiet'
+        mget.quiet    = true
+      when '--version'
+        Mget.version()
+    end
+  end
+rescue GetoptLong::InvalidOption
+  Mget.help()
+end
+mget.target = ARGV[0] unless mget.fromFile?
+
+mget.run()
