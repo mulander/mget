1e6fcf5b11164f358ec74b040cc44a6efbc217fb
defc0n.defc0n <defc0n.defc0n@1632104f-0320-0410-8905-c31b79b9ab67>
2008-06-10 17:10:40 +0000

    Fixed support for patrz.pl (#56, #57)

--- lib/mget/patrz.rb
+++ lib/mget/patrz.rb
@@ -21,11 +21,40 @@ require 'mget/movie_site'
 
 class Patrz < MovieSite
   def get()
-    text = `wget -O- "#{@url}"`
-    text.each_line do |line|
-      if line =~ /patrz\.pl\/static\/flash\/patrzplay\.swf\?file=(.+?)&amp;typ=1&amp;server=(.+?)&/
-        return "http://www" + $2 + ".patrz.pl/uplx/5flv/" + $1 + ".flv"
+    def cookie(url)
+      if url =~ /http:\/\/(.+?)(\/.+?)$/
+        http = Net::HTTP.new($1, 80)
+        data = http.get($2, nil)
+
+        if data['location'] != nil && data['location'] != @url
+          cookie(data['location'])
+        else
+          return data['set-cookie'].to_s
+        end
       end
     end
+
+    if @url =~ /http:\/\/.+?\.patrz\.pl\/$/
+      http = Net::HTTP.new(URI.parse(@url).host, 80)
+      data = http.get('/', nil)
+      @url = data['location']
+    end
+
+    www = URI.parse(@url)
+    cookies = cookie(@url)
+    http = Net::HTTP.new(www.host, 80)
+    http.get(www.path, { 'Cookie' => cookies }) do |line|
+      if line =~ /fid=(.+?);/
+        file = $1
+
+        case @url
+          when /filmy/
+            return 'http://www27.patrz.pl/uplx/5flv/' + file + '.flv'
+          when /mp3/
+            @suffix = '.mp3'
+            return 'http://www27.patrz.pl/uplx/5fla/' + file + '.flva'
+        end
+      end
+    end 
   end
 end
